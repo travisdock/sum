db8c900b2a69a173d8b039c0b4c599b37c2fd6ce
Travis D <travisdock@gmail.com>
2022-12-26 17:29:02 -0700

    Add navbar :satellite:

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  resources :entries
   resources :categories
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "categories#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'entries#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
