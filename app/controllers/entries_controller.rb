a15ca3ac815d1f9c5506e015fe79ee67fa0a68c8
Travis D <travisdock@gmail.com>
2023-01-01 08:10:17 -0700

    Redirect create back to new

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -40,7 +40,7 @@ class EntriesController < ApplicationController
 
     respond_to do |format|
       if @entry.save
-        format.html { redirect_to entry_url(@entry), notice: "Entry was successfully created." }
+        format.html { redirect_to new_entry_url, notice: "Entry was successfully created." }
         format.json { render :show, status: :created, location: @entry }
       else
         format.html { render :new, status: :unprocessable_entity }
