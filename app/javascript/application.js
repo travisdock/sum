2829c737e7099ab8c39501bc035f8e41a5746a6f
Travis D <travisdock@gmail.com>
2023-01-04 05:42:14 -0700

    Add Chartkick

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,11 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
+// bootstrap
 import "jquery"
 import "bootstrap"
 import "plugins/jquery.dataTables.min"
+
+// chartkick
+import "chartkick"
+import "Chart.bundle"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,6 +7,11 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/plugins", under: "plugins"
 
+# Bootstrap
 pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
+
+# Chartkick
+pin "chartkick", to: "chartkick.js"
+pin "Chart.bundle", to: "Chart.bundle.js"
