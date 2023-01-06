3a7da620065d871ba025afaf403a07f701554b73
Travis D <travisdock@gmail.com>
2023-01-06 08:55:00 -0700

    Add order to years dropdown on filter

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,7 +7,7 @@ class EntriesController < ApplicationController
   # GET /entries or /entries.json
   def index
     @entries = Entry.where(user: current_user).by_year(params['year'] || current_user.year_view).includes(:category)
-    @years = Entry.where(user: current_user).pluck(:date).uniq { |d| d.year }.map(&:year)
+    @years = Entry.where(user: current_user).order(:date).pluck(:date).uniq { |d| d.year }.map(&:year)
   end
 
   # POST /filtered_entries
