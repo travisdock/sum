d01086a3c033f88b4c208c95277b0bd247b8ab14
Travis D <travisdock@gmail.com>
2023-03-04 10:05:56 -0700

    Fix tags association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,17 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :categories
   has_many :entries
-  has_many :tags, through: :entries
   before_destroy :remove_data
+
+  def tags
+    # was going to use has_many through but it was not working
+    tag_ids = entries.pluck(:tag_id).uniq
+    Tag.where(id: tag_ids)
+  end
 
   def remove_data
     self.tags.destroy_all
     self.entries.destroy_all
     self.categories.destroy_all
   end
-
-  #def tags
-  #  tag_ids = entries.pluck(:tag_id).uniq
-  #  Tag.where(id: tag_ids)
-  #end
 end
