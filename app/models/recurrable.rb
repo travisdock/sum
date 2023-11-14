2186c547a6fae63e0bd821d8d9342c71219b049d
Name <email>
2023-11-14 05:39:48 -0700

    Add Sentry cron checkin for recurrable arask job

--- app/models/recurrable.rb
+++ app/models/recurrable.rb
@@ -18,11 +18,17 @@ class Recurrable < ApplicationRecord
   end
 
   def self.create_occurrences
+    # Sentry cron mon checkin
+    check_in_id = Sentry.capture_check_in('sum-recurring', :in_progress)
+
     where(day_of_month: Date.today.day).each do |recurrable|
       unless Entry.where(user_id: recurrable.user_id, category_id: recurrable.category_id, date: Date.today, amount: recurrable.amount, notes: recurrable.notes).exists?
         recurrable.create_occurrence
       end
     end
+
+    # Sentry cron mon checkin
+    Sentry.capture_check_in('sum-recurring', :ok, check_in_id: check_in_id)
   end
 
   def create_occurrence
