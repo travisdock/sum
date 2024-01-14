4b166aa96bdda44d28aca4c786606a74d1f73bdf
Name <email>
2024-01-14 07:15:13 -0700

    Make tests pass

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,8 +4,8 @@ class Entry < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  scope :by_year, -> (year) { where("strftime('%Y', date) = ?", year) if year.present? }
-  scope :by_month, -> (month) { where("ltrim(strftime('%m', date), '0') = ?", month) unless month.blank? || month.to_i.zero? }
+  scope :by_year, -> (year) { where("strftime('%Y', date) = ?", year.to_s) if year.present? }
+  scope :by_month, -> (month) { where("ltrim(strftime('%m', date), '0') = ?", month.to_s) unless month.blank? || month.to_i.zero? }
   scope :by_income, -> (income) { joins(:category).where(categories: { income: income }) if !!income == income }
   scope :by_untracked, -> (untracked) { joins(:category).where(categories: { untracked: untracked }) if !!untracked == untracked }
   scope :by_category_id, -> (category_id) { where(category_id: category_id) if category_id.present? }
