e11ea2fe72b3c0e9536d65e79a5c581a5f3cb6b8
Name <email>
2025-06-29 09:00:56 -0600

    Add back in docker whitelist for web console

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_console.permissions = '192.168.0.0/16'
+
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
