16c0950b83ff026e7caa3d8a10a599664f5f3ace
Developer <developer@example.com>
2025-07-12 18:58:26 -0400

    Consolidate migrations a bit

--- db/migrate/20250705153522_migrate_from_devise_to_vanilla_auth.rb
+++ db/migrate/20250705153522_migrate_from_devise_to_vanilla_auth.rb
@@ -2,12 +2,11 @@ class MigrateFromDeviseToVanillaAuth < ActiveRecord::Migration[8.0]
   def change
     # Rename encrypted_password to password_digest for has_secure_password
     rename_column :users, :encrypted_password, :password_digest
-    
+    rename_column :users, :email, :email_address
+
     # Remove Devise-specific columns we won't need
     remove_column :users, :reset_password_token, :string
     remove_column :users, :reset_password_sent_at, :datetime
-    
-    # Keep remember_created_at for remember me functionality
-    # Keep email with its unique index
+    remove_column :users, :remember_created_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_12_202054) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_12_201959) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.boolean "income"
