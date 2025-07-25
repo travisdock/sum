3c7e1b4de9c9a7c36c375ecab36af84c3ff0b746
Travis <36681963+travisdock@users.noreply.github.com>
2025-07-25 16:32:41 -0600

    Working p&l chart (#196)

--- config/importmap.rb
+++ config/importmap.rb
@@ -13,3 +13,7 @@ pin_all_from "app/javascript/plugins", under: "plugins"
 #pin "Chart.bundle", to: "Chart.bundle.js"
 pin "@stimulus-components/notification", to: "@stimulus-components--notification.js" # @3.0.0
 pin "stimulus-use" # @0.52.2
+
+# Chart.js
+pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.5.0/dist/chart.js"
+pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.4/dist/color.esm.js"
