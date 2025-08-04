2db54687575ffe3bdc53a4e202baa41bd5107f76
Developer <developer@example.com>
2025-08-03 19:10:37 -0600

    Use rails credentials for mission control auth

--- app/controllers/mission_control_controller.rb
+++ app/controllers/mission_control_controller.rb
@@ -1,12 +1,2 @@
 class MissionControlController < ApplicationController
-  before_action :ensure_admin
-
-  http_basic_authenticate_with(
-    name: 'admin',
-    password: 'admin'
-  )
-
-  def ensure_admin
-    redirect_to root_path unless current_user&.id == 1
-  end
 end
