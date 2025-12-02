79d2d9475bbb30e9f4166f9999285cb825e6db01
Developer <developer@example.com>
2025-12-01 18:07:51 -0700

    Turn off active storage image variants

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Sum
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_storage.variant_processor = :disabled
   end
 end
